1d33212e30dbd4c012f132d05c4e2a3d43839edc
Phaedrus1301 <128241642+Phaedrus1301@users.noreply.github.com>
2025-11-12 16:10:15 +0530

    End of section 2

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,22 @@
+﻿using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+
+
+namespace AuctionService.RequestHelpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Auction, AuctionDto>()
+                .IncludeMembers(x => x.Item); //When mapping from Auction to AuctionDto, also include members from the related Item entity
+            CreateMap<Item, AuctionDto>(); //helper for line above: explains how to map Item
+
+            CreateMap<CreateAuctionDto, Auction>()
+                .ForMember(d => d.Item, o => o.MapFrom(s => s)); //When creating a new Auction from a CreateAuctionDto, map the Item property from the same source
+            CreateMap<CreateAuctionDto, Item>(); //helper for line above: explains how to map Item
+        }
+    }
+}
